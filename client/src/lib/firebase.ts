e470834ba2c729d724cf04c1d57735e5358b1155
Ayush Singla <101383376+Ayush1665@users.noreply.github.com>
2026-02-18 19:28:43 +0530

    Update firebase.ts

--- client/src/lib/firebase.ts
+++ client/src/lib/firebase.ts
@@ -1,18 +1,1 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAICiJOT5FazmTDz0Kcb4p9K3-HqpZkjCk",
-  authDomain: "design-brew.firebaseapp.com",
-  projectId: "design-brew",
-  storageBucket: "design-brew.firebasestorage.app",
-  messagingSenderId: "137376975563",
-  appId: "1:137376975563:web:0a1e033c63c9ba7da3a94e",
-  measurementId: "G-M8NLNG99KL"
-};
-
-const app = initializeApp(firebaseConfig);
-
-export const db = getFirestore(app);
-export const analytics = getAnalytics(app);
+// No use
